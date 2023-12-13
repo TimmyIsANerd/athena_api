943179a411732819844e9397f6bc281a61af7328
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-12-13 08:54:59 +0100

    add: model to create user profile

---
+++ api/models/Profile.js
@@ -0,0 +1,45 @@
+/**
+ * Profile.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    user: {
+      collection: "user",
+      via: "profile",
+    },
+
+    firstName: {
+      type: "string",
+      required: true,
+      description: "User's first name",
+      example: "John",
+    },
+
+    lastName: {
+      type: "string",
+      required: true,
+      description: "User's last name",
+      example: "Doe",
+    },
+
+    billingAddress: {
+      type: "string",
+      required: true,
+      description: "User's Description",
+    },
+
+    avatarImageURL: {
+      type: "string",
+      description: "User's Avatar Image URL",
+    },
+
+    avatarImagePublicId: {
+      type: "string",
+      description: "User's Avatar Image Public ID",
+    },
+  },
+};
