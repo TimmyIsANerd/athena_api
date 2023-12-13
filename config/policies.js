8c193a91118fd1f63ccbc2eb3ee88403ca28f01d
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-12-13 20:47:39 +0100

    update: set up policy

--- config/policies.js
+++ config/policies.js
@@ -8,15 +8,18 @@
  * https://sailsjs.com/docs/concepts/policies
  */
 
+const isAuth = require("../api/policies/isAuth");
+
 module.exports.policies = {
-
   /***************************************************************************
-  *                                                                          *
-  * Default policy for all controllers and actions, unless overridden.       *
-  * (`true` allows public access)                                            *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * Default policy for all controllers and actions, unless overridden.       *
+   * (`true` allows public access)                                            *
+   *                                                                          *
+   ***************************************************************************/
 
-  // '*': true,
-
+  home: true,
+  "*": isAuth,
+  "auth/signup": true,
+  "auth/login": true,
 };
