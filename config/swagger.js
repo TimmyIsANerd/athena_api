b94beda551d5f9abb21b8bb23dd09e8712ce2595
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-12-14 09:56:09 +0100

    update: new swagger docs settings

--- config/swagger.js
+++ config/swagger.js
@@ -1,11 +1,52 @@
-module.exports.swagger = {
-  swaggerDefinition: {
-    openapi: "3.0.0",
+module.exports.swaggerConfig = {
+  defaults: {
+    responses: {
+      200: {
+        description: "The requested resource",
+      },
+      201:{
+        description:"New Resource Created"
+      },
+      400: {
+        description: "Bad Request",
+      },
+      404: {
+        description: "Resource not found",
+      },
+      500: {
+        description: "Internal server error",
+      },
+    },
+    security: [
+      {
+        Authorization: ["Bearer "],
+      },
+    ],
+  },
+  swagger: {
     info: {
-      title: "Athena Hub REST API Documentation",
+      title: "Athena REST API",
+      description:
+        "This is a generated swagger json to make integration seamless",
+      contact: {
+        name: "Adefeyitimi Gbolahan Adeyeloja",
+        url: "https://github.com/TimmyIsANerd",
+        email: "adefeyitimi@gmail.com",
+      },
+      license: {
+        name: "Apache 2.0",
+        url: "http://www.apache.org/licenses/LICENSE-2.0.html",
+      },
       version: "1.0.0",
-      description: "Documented Endpoints for Athena Hub Edu-Tech REST API Project",
+    },
+    securityDefinitions: {
+      Authorization: {
+        type: "apiKey",
+        description: "user JWT Auth Token",
+        name: "Authorization",
+        in: "header",
+        flow: "password",
+      },
     },
   },
-  apis: ["./api/controllers/*.js", "./api/models/*.js"], 
 };
